e5deeeaa5d9c899b76d2fc60887008e9ea02c1b2
rapeepatph <rapeepat79@gmail.com>
2016-11-01 10:24:07 +0700

    blue&red points & ADSB is added

---
+++ SUREF.web/Controllers/MapController.cs
@@ -0,0 +1,31 @@
+﻿using SUREF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SUREF.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult getAdsb()
+        {
+            ADSB app = new ADSB();
+            List<ADSBModel> result = new List<ADSBModel>();
+            string path = ControllerContext.HttpContext.Server.MapPath("~/DataConfig/ADSBPosition.xml");
+            result= app.GetAdsbData(path);
+            if (result == null)
+            {
+                return Json(new  { status = false}, JsonRequestBehavior.AllowGet);
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
